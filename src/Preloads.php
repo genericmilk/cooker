5c943f19d82de91e51a64a5b996dee91a82b7d51
Peter Day <peterday.main@gmail.com>
2021-03-07 12:31:16 +0000

    Preloads

--- src/Preloads.php
+++ src/Preloads.php
@@ -6,81 +6,64 @@ use App\Http\Controllers\Controller;
 use Genericmilk\Telephone\Telephone;
 use Storage;
 use Exception;
-use Cache;
+use Illuminate\Support\Facades\Cache;
+
 
 class Preloads extends Controller
 {
     public static function obtain($preloads,$oven){
-        foreach($preloads as $preload){
-            Preloads::validatePreload($preload,$oven->format);
-
-        }
         $o = '';
-        dd($preloads,$oven);
-        foreach($frameworks as $f){
-            if ( !file_exists( storage_path('app/cooker_frameworks_cache/'.$f) ) && !is_dir( storage_path('app/cooker_frameworks_cache/'.$f) ) ) {
-                // Need to download
-                $package = Telephone::call('http://registry.npmjs.org/'.$f)->get();
-                $tar = (end($package->versions)->dist->tarball);
-                Storage::put('cooker_frameworks_cache/'.$f.'.tgz', file_get_contents($tar));            
-
-                $p = new \PharData(storage_path('app/cooker_frameworks_cache/'.$f.'.tgz'));
-                $p->extractTo(storage_path('app/cooker_frameworks_cache/'.$f.'_download'));
-                unlink(storage_path('app/cooker_frameworks_cache/'.$f.'.tgz'));
-                rename(storage_path('app/cooker_frameworks_cache/'.$f.'_download/package/dist'),storage_path('app/cooker_frameworks_cache/'.$f));
-                $this->delete_files(storage_path('app/cooker_frameworks_cache/'.$f.'_download'));
-            }
- 
-            
-            $dir = scandir(storage_path('app/cooker_frameworks_cache/'.$f));
-			unset($dir[0]);
-			unset($dir[1]);
-			if (($key = array_search('.DS_Store', $dir)) !== false) {
-				unset($dir[$key]);
-			}
-            $dir = array_values($dir);
-			foreach($dir as $framework){                
-                $ext = !$dev ? '.min.'.$type : '.'.$type;                
-                if((pathinfo($framework, PATHINFO_EXTENSION))==$type){
-                    if (strpos($framework, '.min.') !== false) {
-                        if(!$dev){
-                            $o .= file_get_contents(storage_path('app/cooker_frameworks_cache/'.$f.'/'.$framework));
-                        }
-                    }else{
-                        if($dev){
-                            $o .= file_get_contents(storage_path('app/cooker_frameworks_cache/'.$f.'/'.$framework));
-                        }
-                    }
-                }
-                
-            }
+        foreach($preloads as $preload){
+            $o .= Preloads::validatePreload($preload,$oven->format);
         }
         return $o;
     }
     public static function validatePreload($p,$t){
+        $ext = pathinfo($p, PATHINFO_EXTENSION);
+        if($ext!=$t){
+            throw new Exception('Cooker: Mismatching type of file for oven format on preload. '.$p.' did not pass validation');
+        }
+        
         if (strpos($p, '://') !== false) {
             // Remote url
             if (strpos($p, 'http') === false) {
                 throw new Exception('Cooker: Remote url provided for preload but no protocol provided. Please provide at least http or https. '.$p.' did not pass validation');
             }
-            $ext = pathinfo($p, PATHINFO_EXTENSION);
-            if($ext!=$t){
-                throw new Exception('Cooker: Mismatching type of file for oven format on preload. '.$p.' did not pass validation');
+            
+
+            $cache_name = 'cooker-'.md5($p);
+            if (!Cache::has($cache_name)){
+                try{
+                    $data = file_get_contents($p);
+                }catch(\Exception $e){
+                    throw new Exception('Cooker: Could not download remote file. '.$p.' did not pass validation');
+                }
+                Cache::forever($cache_name, $data);
+                $o = $data;   
+            }else{
+                $o = Cache::get($cache_name);    
+            }
+
+        }else{
+            if(!file_exists($p)){
+                throw new Exception('Cooker: Local preload file could not be found. '.$p.' did not pass validation');
+            }
+
+            $cache_name = 'cooker-'.md5($p);
+            if (!Cache::has($cache_name)){
+                try{
+                    $data = file_get_contents($p);
+                }catch(\Exception $e){
+                    throw new Exception('Cooker: Could not read local file contents. '.$p.' did not pass validation');
+                }
+    
+                Cache::forever($cache_name, $data);
+                $o = $data;   
+            }else{
+                $o = Cache::get($cache_name);    
             }
         }
-    }
-    private function delete_files($target) {
-        if(is_dir($target)){
-            $files = glob( $target . '*', GLOB_MARK ); //GLOB_MARK adds a slash to directories returned
-            foreach( $files as $file ){
-                $this->delete_files( $file );      
-            }
-            try{
-                rmdir( $target );
-            }catch(\Exception $e){
-            }
-        } elseif(is_file($target)) {
-            unlink( $target );  
-        }
+        
+        return $o;
     }
 }
