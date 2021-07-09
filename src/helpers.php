3afe87ebe8e7b2c220bea76490cac07eb2cf955c
Peter Day <peterday.main@gmail.com>
2021-07-09 21:40:35 +0100

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,13 @@
 <?php
 
 function cooker_resource($file){
+    if (!file_exists(public_path('build'))){
+        return null;
+    }
+    if (!file_exists(public_path('build/'.$file))){
+         return null;
+    }
+    
     $hash = config('app.debug') ? time() : md5(file_get_contents(public_path('build/'.$file)));
     $url = '/build/'.$file.'?build=' . $hash;
 
