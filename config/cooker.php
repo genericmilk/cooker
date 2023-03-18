5dbb6a180b0b020f24c78b59709b063f35185c6d
Peter Day <peterday.main@gmail.com>
2023-03-18 23:00:59 +0000

    More preps

--- config/cooker.php
+++ config/cooker.php
@@ -41,13 +41,40 @@ return [
     */
     'canSpeedyCook' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cooker Package Manager
+    |--------------------------------------------------------------------------
+    |
+    | The settings for the Cooker Package Manager. This is used to install
+    | and update packages from NPM-like repositories.
+    |
+    */
+    'packageManager' => [
+        'packagesList' => env('COOKER_PACKAGE_JSON_LOCATION', base_path('cooker.json')),
+        'packagesPath' => env('COOKER_PACKAGE_PATH', base_path('cooker_packages')),
+        'packageManager' => env('COOKER_PACKAGE_MANAGER', 'npm'),
+    ],
+
     
     'ovens' => [
         [
             'cooker' => 'Genericmilk\Cooker\Ovens\Less',
             'preload' => [
                 /*
-                    Place any CDNs or local paths in the array here, These will be injected and not parsed before the appcode
+                    Place any CDNs or local paths in the array here.
+                    These files will be injected and not parsed before the appcode
+                    These entires can be either a string for both dev and prod or an array with dev and prod keys
+                    For example
+
+                    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+
+                    OR
+
+                    [
+                        'dev' => 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+                        'prod' => 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+                    ]
                 */
             ],
             'input' => [
@@ -61,7 +88,19 @@ return [
             'cooker' => 'Genericmilk\Cooker\Ovens\Js',
             'preload' => [
                 /*
-                    Place any CDNs or local paths in the array here, These will be injected and not parsed before the appcode
+                    Place any CDNs or local paths in the array here.
+                    These files will be injected and not parsed before the appcode
+                    These entires can be either a string for both dev and prod or an array with dev and prod keys
+                    For example
+
+                    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+
+                    OR
+
+                    [
+                        'dev' => 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+                        'prod' => 'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+                    ]
                 */
                 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js'
             ],
