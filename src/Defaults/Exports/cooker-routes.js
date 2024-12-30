bc776bccf29c2b391b5480635c778183c6ede8d8
Peter Day <peterday.main@gmail.com>
2024-12-30 22:07:14 +0000

    Enhance cookerRoutes class to improve path matching logic and return boolean for boot status

--- src/Defaults/Exports/cooker-routes.js
+++ src/Defaults/Exports/cooker-routes.js
@@ -3,15 +3,16 @@ class cookerRoutes{
         this.routes = [];
 
         console.log(this.routes);
+    };
 
+    canBoot(className){
         // go round each of this.routes and check the key matches window.location.pathname - wildcards are allowed
         for(let i = 0; i < this.routes.length; i++){
             let path = this.routes[i].path;
-            let className = this.routes[i].class;
 
             // is the path just a wildcard? if so we can just run the class
             if(path === '*'){
-                new Application();
+                return true;
             }else{
                 // does the path contain a wildcard?
                 if(path.includes('*')){
@@ -21,20 +22,21 @@ class cookerRoutes{
                     let pathEnd = pathParts[1];
 
                     if(window.location.pathname.startsWith(pathStart) && window.location.pathname.endsWith(pathEnd)){
-                        new Application();
+                        return true;
                     }
                 }else{
                     // no wildcard, just match the path
                     if(path === window.location.pathname){
-                        new Application();
+                        return true;
                     }
                 }
             }
         }
 
-    }
+        return false;
+    };
+
 };
 
 new cookerRoutes();
-
 export default cookerRoutes;
