cc11f8ad0daa0e5c5a54e0748da314b0a63ca724
Peter Day <peterday.main@gmail.com>
2024-12-31 21:15:24 +0000

    Add Pest testing framework and initial test setup

---
+++ Pest.php
@@ -0,0 +1,14 @@
+<?php
+
+use Pest\Plugin;
+
+Plugin::load();
+
+beforeEach(function () {
+    // Load application environment or any necessary setup
+});
+
+function globalHelper()
+{
+    // Define global test helpers
+}
---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,5 @@
+<?php
+
+it('works', function () {
+    expect(true)->toBeTrue();
+});
