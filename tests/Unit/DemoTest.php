65c5d3ce8fca1d55121175e2e0b27e6149924cd4
Peter Day <peterday.main@gmail.com>
2024-12-31 22:06:20 +0000

    Refactor DemoTest to use class-based structure and add a basic test example

--- tests/Unit/DemoTest.php
+++ tests/Unit/DemoTest.php
@@ -1,5 +1,19 @@
 <?php
 
-it('works', function () {
-    expect(true)->toBeTrue();
-});
+namespace Tests\Unit;
+ 
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CookerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+}
